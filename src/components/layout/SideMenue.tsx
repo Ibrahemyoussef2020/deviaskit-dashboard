59639293051b59b29cc95338250e36e754e9b5b1
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2024-09-17 17:46:12 +0400

    initial commit

---
+++ src/components/layout/SideMenue.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SideMenue = () => {
+  return (
+    <ul>
+        <li>Analists</li>
+        <li>Profile</li>
+        <li>Setteings</li>
+    </ul>
+  )
+}
+
+export default SideMenue
